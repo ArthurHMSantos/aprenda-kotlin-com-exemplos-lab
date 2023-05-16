68dedd8bbc4fe7c66422909ce42f1825038b81b3
Arthur Henrique <101672583+ArthurHMSantos@users.noreply.github.com>
2023-05-15 23:47:48 -0300

    Update desafio.kt
    - Adicionado um construtor padrão vazio à classe ConteudoEducacional, permitindo criar objetos com apenas o nome e a duração padrão;
    - Alterado a propriedade conteudos da classe Formacao para ser uma lista mutável (MutableList) em vez de uma lista imutável (List);
    - Na função matricular da classe Formacao, substituí o "TODO" por inscritos.add(usuario), que adiciona o usuário à lista de inscritos;
    - No main, criei alguns objetos de exemplo: conteudo1, conteudo2 e conteudo3. Em seguida, uma formação chamada formacao, na qual atribuí os conteúdos a ela e matriculei dois usuários. Por fim, printei as informações da formação, conteúdos e inscritos.

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,48 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
 class Usuario
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+class Formacao(val nome: String, var conteudos: MutableList<ConteudoEducacional> = mutableListOf()) {
 
     val inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    // Criando objetos de exemplo para ConteudoEducacional
+    val conteudo1 = ConteudoEducacional("Introdução ao Kotlin", 120)
+    val conteudo2 = ConteudoEducacional("Programação Orientada a Objetos em Kotlin")
+    val conteudo3 = ConteudoEducacional("Kotlin Avançado", 90)
+
+    // Criando um objeto de Formacao
+    val formacao = Formacao("Formação Kotlin")
+
+    // Adicionando os conteúdos à formação
+    formacao.conteudos.add(conteudo1)
+    formacao.conteudos.add(conteudo2)
+    formacao.conteudos.add(conteudo3)
+
+    // Criando usuários
+    val usuario1 = Usuario()
+    val usuario2 = Usuario()
+
+    // Matriculando os usuários na formação
+    formacao.matricular(usuario1)
+    formacao.matricular(usuario2)
+
+    // Imprimindo as informações da formação, conteúdos e inscritos
+    println("Formação: ${formacao.nome}")
+    println("Conteúdos:")
+    for (conteudo in formacao.conteudos) {
+        println("- ${conteudo.nome} (${conteudo.duracao} minutos)")
+    }
+    println("Inscritos:")
+    for (inscrito in formacao.inscritos) {
+        println("- $inscrito")
+    }
 }
